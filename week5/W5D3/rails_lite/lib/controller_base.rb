557460499f397e6b2622f62502a7b7987a81e078
mwojick <35327565+mwojick@users.noreply.github.com>
2018-05-16 18:04:49 -0700

    Add w5d3

---
+++ week5/W5D3/rails_lite/lib/controller_base.rb
@@ -0,0 +1,63 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "Already rendered" if already_built_response?
+    @already_built_response = true
+    @res.location = url
+    @res.status = 302
+
+    @session.store_session(@res)
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "Already rendered" if already_built_response?
+    @already_built_response = true
+    @res['Content-Type'] = content_type
+    @res.write(content)
+
+    @session.store_session(@res)
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = "#{self.class}".underscore
+
+    file_name = "views/#{controller_name}/#{template_name}.html.erb"
+    file = File.read(file_name)
+
+    content = ERB.new(file).result(binding)
+    render_content(content,'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
